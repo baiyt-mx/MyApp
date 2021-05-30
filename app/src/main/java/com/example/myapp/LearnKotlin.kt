761a230ee507e1644998d4346058ef796a2d3118
baiyt <1035955292@qq.com>
2021-05-30 19:54:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapp/LearnKotlin.kt
@@ -0,0 +1,12 @@
+package com.example.myapp
+
+import java.sql.DriverManager.println
+
+/**
+ * Created by bai on 2021/5/30.
+ */
+class LearnKotlin {
+    fun main(){
+        println("Hello Kotlin!")
+    }
+}
